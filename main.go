163b6b1b61687f042a4dd84cbb61fe503e441649
SubZiro <webnlevel@gmail.com>
2020-03-26 23:06:05 +0400

    Update main.go

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ import (
 )
 
 var (
-    k int = 5 //кол-во "потоков"
-    q string = "go" //строка поиска
+	k int = 5 //кол-во "потоков"
+	q string = "go" //строка поиска
 )
 
 func init() {
@@ -36,8 +36,7 @@ type counter struct {
 }
 
 func new_counter() *counter  {
-	//создание экз счетчика
-	return &counter{lock:&sync.Mutex{}}  
+	return &counter{lock:&sync.Mutex{}}  //создание экз счетчика
 }
 
 func (c *counter) count_add(count int) {
@@ -52,13 +51,8 @@ func (c *counter) count_get() int {
 	return c.count  // возврат общего количества найденых вхождений
 }
 
-func count_q_inbody(string_html, q string) int {
-	// подсчет количества вхождений строки в полученый запрос
-	return strings.Count(strings.ToLower(string_html), q)
-}
-
 func scaner_urls() []string {
-	// полкчение массива ссылок передаваемых при запуске программы
+	// получение массива ссылок передаваемых при запуске программы
 	a := []string{}
 	buf := bufio.NewScanner(os.Stdin)
 	for {
@@ -71,6 +65,11 @@ func scaner_urls() []string {
 	return a[1:]
 }
 
+func count_q_inbody(string_html, q string) int {
+	// подсчет количества вхождений строки в полученый запрос
+	return strings.Count(strings.ToLower(string_html), q)
+}
+
 func get_url(url string) (*string, error) {
 	// get запрос на URL, возвращает содержимое htlm страницы
 	response, err := http.Get(url)
@@ -79,6 +78,7 @@ func get_url(url string) (*string, error) {
 		os.Exit(1)
 	}
 	defer response.Body.Close()  // закрытие
+	//s, err := io.Copy(os.Stdout, resp.Body)
 	contents, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		fmt.Printf("%s", err)
@@ -88,34 +88,35 @@ func get_url(url string) (*string, error) {
 	return &result, nil
 }
 
-
-func parse(urls []string, q string, k int,  wg *sync.WaitGroup) {
+func parse(urls []string, q string, k int, wg *sync.WaitGroup) {
 	// парсинг всех url через горутины передача через канал
 	counter := new_counter()
+	stackChan := make(chan int, k)
 	for _, url := range urls {
 		wg.Add(1)  // инкриментирование стека горутин
-		go parse_one_url(url, q, wg)
+		go parse_one_url(url, ProducerObject{StackChannel:stackChan, Counter:counter, Q:q}, wg)
 	}
+	close(stackChan)
 	wg.Wait()   // ожидание закрытия всех потоков
 	fmt.Printf("Total: %d\n", counter.count_get())
 }
 
-func parse_one_url(url string, q string, wg *sync.WaitGroup)  {
+func parse_one_url(url string, pobj ProducerObject, wg *sync.WaitGroup) {
 	// парсинг одного url
 	defer wg.Done()  //вычитаем стек потоков
 	contents, err := get_url(url)
 	if err != nil {
 		return
 	}
-	count_i := count_q_inbody(*contents, q)
-	counter.count_add(count_i)
+	count_i := count_q_inbody(*contents, pobj.Q)  // количество вхождений
+	pobj.Counter.count_add(count_i)  // добавление количества вхождений в счетчик
 	fmt.Printf("Count '%s' for %s : %d\n", q, url, count_i)
 }
-
 
 func main() {
 	flag.Parse() //И запускаем разбор аргументов
 	fmt.Println("========================")
+
 	urls := scaner_urls()
 	fmt.Printf("Получено %d url\n", len(urls))
 	wg := new(sync.WaitGroup)  // индикатор завершения потоков
